fcef2ae5b209f4c830d3967b0f92db90e43a781b
ancmsvn <me@clgt.io>
2018-04-17 04:01:14 +0700

    Methor.handlerResponse: support array

--- src/methor.js
+++ src/methor.js
@@ -5,16 +5,16 @@ import regeneratorRuntime from 'regenerator-runtime'
 import { createServer, Server } from 'http'
 import chalk from 'chalk'
 import {
-	isFunction,
-	isObject,
-	isArray,
-	isNumber,
-	isPromise,
-	isString,
-	getProperty,
-	getAllKeys,
-	capitalize,
-	isNull
+  isFunction,
+  isObject,
+  isArray,
+  isNumber,
+  isPromise,
+  isString,
+  getProperty,
+  getAllKeys,
+  capitalize,
+  isNull
 } from './util'
 import * as util from './util'
 import Router from 'router'
@@ -30,63 +30,63 @@ import Restserver from './restserver'
 import { resolve } from 'path'
 
 function Methor(opts) {
-	if (!(this instanceof Methor)) return new Methor(opts)
+  if (!(this instanceof Methor)) return new Methor(opts)
 
-	if (!opts) throw new TypeError('1 argument required, but only 0 present.')
-	if (!isObject(opts)) throw new TypeError('argument options must be object')
+  if (!opts) throw new TypeError('1 argument required, but only 0 present.')
+  if (!isObject(opts)) throw new TypeError('argument options must be object')
 
-	const methods = opts.methods
+  const methods = opts.methods
 
-	if (!methods) throw new TypeError('option methods is required')
-	if (!(isFunction(methods) || isObject(methods)))
-		throw new TypeError('option methods must be a function or object ')
+  if (!methods) throw new TypeError('option methods is required')
+  if (!(isFunction(methods) || isObject(methods)))
+    throw new TypeError('option methods must be a function or object ')
 
-	// if (!opts.port) throw new TypeError('option port is required')
-	// if (!isNumber(opts.port)) throw new TypeError('option port must be number')
+  // if (!opts.port) throw new TypeError('option port is required')
+  // if (!isNumber(opts.port)) throw new TypeError('option port must be number')
 
-	this.opts = opts
-	this.methods = getAllKeys(methods).reduce((obj, methodName) => {
-		obj[methodName] = getProperty(methods, methodName)
-		return obj
-	}, {})
+  this.opts = opts
+  this.methods = getAllKeys(methods).reduce((obj, methodName) => {
+    obj[methodName] = getProperty(methods, methodName)
+    return obj
+  }, {})
 
-	if (opts.funcs) {
-		if (!isObject(opts.funcs))
-			throw new TypeError('option funcs must be object')
-		this.funcs = opts.funcs
-	}
+  if (opts.funcs) {
+    if (!isObject(opts.funcs))
+      throw new TypeError('option funcs must be object')
+    this.funcs = opts.funcs
+  }
 
-	// --- private property ---
-	this.restserver = Restserver
-	this.addRoute = addRoute
-	this.$mount = Mount
+  // --- private property ---
+  this.restserver = Restserver
+  this.addRoute = addRoute
+  this.$mount = Mount
 
-	this.$on = function(name, cb) {
-		return this.proxy.on(name, cb)
-	}
-	this.$emit = function(name, ...data) {
-		return this.proxy.emit(name, ...data)
-	}
+  this.$on = function(name, cb) {
+    return this.proxy.on(name, cb)
+  }
+  this.$emit = function(name, ...data) {
+    return this.proxy.emit(name, ...data)
+  }
 
-	this._beforeEnter = []
-	this._installed = []
-	this.services = {}
+  this._beforeEnter = []
+  this._installed = []
+  this.services = {}
 
-	// Server.call(this)
+  // Server.call(this)
 
-	this.installPlugin()
+  this.installPlugin()
 
-	this.init()
-	if (isNumber(opts.port)) {
-		this.listen()
-	}
+  this.init()
+  if (isNumber(opts.port)) {
+    this.listen()
+  }
 
-	if (isNull(opts.port)) {
-		this.server = new Server()
-		this.listen()
-	}
+  if (isNull(opts.port)) {
+    this.server = new Server()
+    this.listen()
+  }
 
-	return this
+  return this
 }
 
 // Methor.prototype = Object.create(Server.prototype)
@@ -97,67 +97,67 @@ Methor.prototype.listen = Listen
 Methor.prototype.middleware = Middleware
 
 Methor.prototype.beforeEnter = function(...callbacks) {
-	if (callbacks.length == 0) throw new TypeError('argument handler is required')
-	for (let callback of callbacks) {
-		if (!isFunction(callback))
-			throw new TypeError('argument handler must be function')
-	}
-	this._beforeEnter = [...this._beforeEnter, ...callbacks]
-	return this
+  if (callbacks.length == 0) throw new TypeError('argument handler is required')
+  for (let callback of callbacks) {
+    if (!isFunction(callback))
+      throw new TypeError('argument handler must be function')
+  }
+  this._beforeEnter = [...this._beforeEnter, ...callbacks]
+  return this
 }
 
 Methor.prototype.handlerResponse = async function(req, res, _, result) {
-	if (isPromise(result)) result = await result
-	if (res.finished) return false
-	if (result == undefined || result == null) return res.end('')
-	if (isString(result) || isNumber(result)) return res.end(String(result))
-	if (isObject(result)) return res.json(result)
-	res.end(result.toString())
+  if (isPromise(result)) result = await result
+  if (res.finished) return false
+  if (result == undefined || result == null) return res.end('')
+  if (isString(result) || isNumber(result)) return res.end(String(result))
+  if (isObject(result) || isArray(result)) return res.json(result)
+  res.end(result.toString())
 }
 
 Methor.prototype.warn = function(msg, plugin) {
-	if (process.env.NODE_ENV != 'development') {
-		console.warn(
-			chalk.yellow('[') +
-				' Methor ' +
-				chalk.yellow.bold('warning') +
-				' ' +
-				(plugin ? 'from ' + chalk.red.bold(plugin) : '') +
-				chalk.yellow(' ]') +
-				' ' +
-				msg
-		)
-	}
+  if (process.env.NODE_ENV != 'development') {
+    console.warn(
+      chalk.yellow('[') +
+        ' Methor ' +
+        chalk.yellow.bold('warning') +
+        ' ' +
+        (plugin ? 'from ' + chalk.red.bold(plugin) : '') +
+        chalk.yellow(' ]') +
+        ' ' +
+        msg
+    )
+  }
 }
 
 Methor.prototype.installPlugin = function() {
-	const plugins = this.opts.plugins
-	if (!plugins) return
-	const _installed = this._installed
-	for (const plugin of plugins) {
-		if (_installed.includes(plugin)) continue
+  const plugins = this.opts.plugins
+  if (!plugins) return
+  const _installed = this._installed
+  for (const plugin of plugins) {
+    if (_installed.includes(plugin)) continue
 
-		_installed.push(plugin)
+    _installed.push(plugin)
 
-		// const args = [...arguments].slice(1)
-		if (isFunction(plugin.install)) {
-			plugin.install.apply(this, [])
-		} else if (isFunction(plugin)) {
-			plugin.apply(this, [])
-		}
-	}
+    // const args = [...arguments].slice(1)
+    if (isFunction(plugin.install)) {
+      plugin.install.apply(this, [])
+    } else if (isFunction(plugin)) {
+      plugin.apply(this, [])
+    }
+  }
 }
 
 for (let key in util) {
-	Methor.prototype[key] = Methor[key] = util[key]
+  Methor.prototype[key] = Methor[key] = util[key]
 }
 
 ;['validator'].map(name => {
-	Object.defineProperty(Methor, capitalize(name), {
-		get() {
-			return require(resolve(__dirname, 'plugins', name)).default
-		}
-	})
+  Object.defineProperty(Methor, capitalize(name), {
+    get() {
+      return require(resolve(__dirname, 'plugins', name)).default
+    }
+  })
 })
 
 module.exports = Methor
