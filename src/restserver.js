a532ba348e964ca63d4a9c529e3130052d09f71f
ancmsvn <me@clgt.io>
2018-04-25 01:45:36 +0700

    support method.$inject

--- src/restserver.js
+++ src/restserver.js
@@ -1,79 +1,74 @@
 'use strict'
 
 import {
-	getParamFunc,
-	getProperty,
-	isString,
-	isFunction,
-	bind,
-	isArray
+  getParamFunc,
+  getProperty,
+  isString,
+  isFunction,
+  bind,
+  isArray
 } from './util'
 
 const generateRegEx = name => {
-	return new RegExp('\\$?' + name + '\\.?(.+)?$')
+  return new RegExp('\\$?' + name + '\\.?(.+)?$')
 }
 
 export default function(...args) {
-	const [req, res, next] = args
-	const methodName = req.query.method
-	const inject = [] // bind to method
+  const [req, res, next] = args
+  const methodName = req.query.method
+  const inject = [] // bind to method
 
   let method = this.methods[methodName]
 
-	if (!method) {
-		this.warn(`method ${methodName} not exist`)
-		return next()
-	}
-
-	const regexs = {
-		req: [generateRegEx('(req|request)'), req],
-		res: [generateRegEx('(res|resp|request)'), res],
-		headers: [generateRegEx('headers'), req.headers],
-		next: [generateRegEx('next'), next]
-	}
-
-	for (let key in this.services) {
-		const service = this.services[key]
-		regexs[key] = [generateRegEx(key), service]
-	}
-
-	const setInject = (param) =>  {
-		for (let key in regexs) {
-			// check all regexs
-			const [regex, obj] = regexs[key] // [0] -> generateRegEx, 1 -> object
-			if (regex.test(param)) {
-				// if ok
-				let matches = param.match(regex),
-            name
-        if (matches && matches[1]) { // if matched, name will be equal last item of matches
-          name = matches[matches.length-1]
-        }
-				if (name) {
-					return inject.push(getProperty(obj, name))
-				}
-				return inject.push(obj)
-			}
-		}
-		inject.push(undefined)
-	}
-
-	if (isFunction(method)) {
-		const params = getParamFunc(method)
-		for (let param of params) {
-			setInject(param)
-		}
-	} else if (isArray(method)) {
-    let params = method.slice(0, -1)
-
-    for (let param of params) {
-      setInject(param)
-    }
-
-    method = method.slice(-1).shift()
-    if (!isFunction(method))
-      throw new TypeError('argument handler is required')
+  if (!method) {
+    this.warn(`method ${methodName} not exist`)
+    return next()
   }
 
-	let result = bind(method, this.opts.funcs, args)(...inject)
-	this.handlerResponse(...args, result)
+  const regexs = {
+    req: [generateRegEx('(req|request)'), req],
+    res: [generateRegEx('(res|resp|request)'), res],
+    headers: [generateRegEx('headers'), req.headers],
+    next: [generateRegEx('next'), next]
+  }
+
+  for (let key in this.services) {
+    const service = this.services[key]
+    regexs[key] = [generateRegEx(key), service]
+  }
+
+  const setInject = param => {
+    for (let key in regexs) {
+      // check all regexs
+      const [regex, obj] = regexs[key] // [0] -> generateRegEx, 1 -> object
+      if (regex.test(param)) {
+        // if ok
+        let matches = param.match(regex),
+          name
+        if (matches && matches[1]) {
+          // if matched, name will be set to matches[last]
+          name = matches[matches.length - 1]
+        }
+        if (name) {
+          return inject.push(getProperty(obj, name))
+        }
+        return inject.push(obj)
+      }
+    }
+    inject.push(undefined)
+  }
+
+  if (isArray(method.$inject)) {
+    method.$inject.map(setInject)
+  } else if (isFunction(method)) {
+    const params = getParamFunc(method)
+    params.map(setInject)
+  } else if (isArray(method)) {
+    method.slice(0, -1).map(setInject)
+    method = method[method.length - 1]
+    if (!isFunction(method)) throw new TypeError('argument handler is required')
+  }
+
+  let result = bind(method, this.opts.funcs, args)(...inject)
+  this.handlerResponse(...args, result)
 }
