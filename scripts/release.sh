fc13aad6caf323b19dfcca2b7ea77a6c209d1db2
ancmsvn <me@clgt.io>
2018-07-09 18:16:27 +0700

    npm publish

--- scripts/release.sh
+++ scripts/release.sh
@@ -32,7 +32,7 @@ select VERSION in patch minor major "Specific Version"
         echo Releasing ${NEW_VERSION} ...
 
         # npm release
-        yarn publish
+        npm publish
         echo "✅ Released to npm."
 
         # github release
